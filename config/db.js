751dcf058e187dc1a377abe0fd94f45de1ffc224
kiranKachhawa <kirankachhawa092@gmail.com>
2026-04-07 20:01:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,26 +1,11 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-let isConnected = false;
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
 
-const connectDB = async () => {
-    if (isConnected) {
-        console.log("=> using existing database connection");
-        return;
-    }
-
-    if (!process.env.MONGO_URI) {
-        console.error("Error: MONGO_URI environment variable is not set.");
-        throw new Error("MONGO_URI not set");
-    }
-
-    try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
-        isConnected = db.connections[0].readyState === 1;
-        console.log("=> using new database connection");
-    } catch (error) {
-        console.error("MongoDB Connection Error:", error);
-        throw error;
-    }
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Database connection error");
+  }
 };
-
-module.exports = connectDB;
